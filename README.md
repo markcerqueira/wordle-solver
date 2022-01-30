00f7a6a07e8aaac3aa81ecfc137a525b9b082b6e
Mark Cerqueira <markcerq@twitch.tv>
2022-01-30 14:26:25 -0800

    Add all English words resource and parse into List

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # wordle-solver
 Template for a Wordle-solving competition
+
+Dictionary text file from https://github.com/dwyl/english-words
--- src/com/wordlesolver/WordleSolver.java
+++ src/com/wordlesolver/WordleSolver.java
@@ -1,8 +1,19 @@
 package com.wordlesolver;
 
+import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.List;
+
 public class WordleSolver {
 
-    public static void main(String[] args) {
-        System.out.println("Testing 1 2 3");
+    public static void main(String[] args) throws Exception {
+        List<String> allEnglishWords = Files.readAllLines(new File("resources/all-english-words.txt").toPath(), Charset.defaultCharset());
+
+        System.out.println("Total words in our dictionary = " + allEnglishWords.size());
+
+        List<String> fiveLetterWords = allEnglishWords.stream().filter(word -> word.length() == 5).toList();
+
+        System.out.println("Total five-letter words in our dictionary = " + fiveLetterWords.size());
     }
 }
